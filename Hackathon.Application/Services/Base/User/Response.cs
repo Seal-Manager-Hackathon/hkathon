8ef9707af6db6232fc3c47d6de20b681a6bb713e
phamquochoang356@gmail.com <phamquochoang356@gmail.com>
2026-07-14 01:37:21 +0700

    push

--- Hackathon.Application/Services/Base/User/Response.cs
+++ Hackathon.Application/Services/Base/User/Response.cs
@@ -23,11 +23,20 @@ public class GetMyProfileResponse
     public string Email { get; set; } = null!;
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
+    public string? PhoneNumber { get; set; }
     public string? AvatarUrl { get; set; }
     public string? Bio { get; set; }
+    public string? Address { get; set; }
+    public DateTimeOffset? DateOfBirth { get; set; }
+    public string? StudentId { get; set; }
+    public string? College { get; set; }
+    public string? ImgUrl { get; set; }
+    public string? LinkUrl { get; set; }
     public string? Role { get; set; }
     public string? Status { get; set; }
+    public string? BanReason { get; set; }
+    public DateTimeOffset? BannedAt { get; set; }
     public bool? IsVerified { get; set; }
-    public string? College { get; set; }
+    public DateTimeOffset? VerifyEmailAt { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
 }
